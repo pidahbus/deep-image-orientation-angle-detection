bbc0d70b49b1839230f74a869f07f151066fc01d
Subhadip Maji <44110256+pidahbus@users.noreply.github.com>
2022-04-22 12:03:33 +0530

    Update loss.py

--- loss.py
+++ loss.py
@@ -10,12 +10,6 @@ def angular_loss_mae(y_true, y_pred):
     :return:
     """
 
-    N = tf.math.divide(y_pred, 360)
-    N = tf.floor(N)
-
-    y_pred = K.switch(K.greater_equal(y_pred, 360), y_pred - (N * 360), y_pred)
-    y_pred = K.switch(K.less(y_pred, 0), y_pred + (K.abs(N) * 360), y_pred)
-
     error1 = K.abs(y_pred - y_true)
     error2 = 360 - error1
     error = K.minimum(error1, error2)
